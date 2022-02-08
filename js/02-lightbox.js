7173cfbfa45365039e2380e8a25aa7609afc80ef
Serhii <89796587+SerhiiSakhno1994@users.noreply.github.com>
2022-02-08 14:07:19 +0200

    task2-done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,21 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const imgContainer = document.querySelector('div.gallery');
+const cardMarkup = creteImgCard(galleryItems);
+
+imgContainer.insertAdjacentHTML('beforeend', cardMarkup)
+
+function creteImgCard(galleryItems) {
+  return galleryItems.map(({preview, original, description}) => {
+    return `
+    <div class="gallery__item">
+      <a class="gallery__item" href="${original}" onclick="event.preventDefault()">
+        <img class="gallery__image" src="${preview}" alt="Image description" title = "${description}" />
+      </a>
+    </div>`;
+  }).join('');
+};
+
+var lightbox = new SimpleLightbox('.gallery a', { /* options */ });
